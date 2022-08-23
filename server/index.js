7eaf3432eb6bd5d46bd4ec3e668ee7d488fc0623
YourRade <you@example.com>
2022-08-23 19:49:37 +0300

    add variable for port

--- server/index.js
+++ server/index.js
@@ -21,16 +21,15 @@ app.use(compression());
 app.use("/api", router);
 app.use(errorMiddleware);
 
+const PORT = process.env.PORT || 5000;
+
 const start = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    app.listen(
-      process.env.PORT || 5000,
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+    app.listen(PORT, console.log(`Server running on port ${PORT}`));
   } catch (e) {
     console.log(e);
   }
